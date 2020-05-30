a63fc6745eec832a5b9209f439e60368ae83b62a
Daniel Foehr <daniel.foehr@sap.com>
2020-05-30 22:40:44 +0200

    Updating for Release 0.0.2

--- switch.rb
+++ switch.rb
@@ -4,18 +4,18 @@ class Switch < Formula
   version "v0.0.1"
 
   if OS.mac?
-    url "https://github.com/danielfoehrKn/kubectlSwitch/releases/download/0.0.1/switch.tar.gz"
-    sha256 "0b02ced79f210ba5407dd184abdd5238134d6c186f526b223ab74a53e183e576"
+    url "https://github.com/danielfoehrKn/kubectlSwitch/releases/download/0.0.2/switch.tar.gz"
+    sha256 "1c0d31b922ed31f303493617d69fa45e67756a193aa88c69f2c478ce12c285d4"
   elsif OS.linux?
-    url "https://github.com/danielfoehrKn/kubectlSwitch/releases/download/0.0.1/switch.tar.gz"
-    sha256 "0b02ced79f210ba5407dd184abdd5238134d6c186f526b223ab74a53e183e576"
+    url "https://github.com/danielfoehrKn/kubectlSwitch/releases/download/0.0.2/switch.tar.gz"
+    sha256 "1c0d31b922ed31f303493617d69fa45e67756a193aa88c69f2c478ce12c285d4"
   end
 
   depends_on "danielfoehrkn/switch/switcher"
   depends_on :arch => :x86_64
 
   def install
-    bin.install "switch.sh" => "switch"
+    prefix.install "switch.sh"
   end
 
   test do
--- switcher.rb
+++ switcher.rb
@@ -1,14 +1,14 @@
 class Switcher < Formula
-  desc "Providing the executable for the 'switch' script"
+  desc "Providing the binary executable for the 'switch' script."
   homepage "https://github.com/danielfoehrKn/kubectlSwitch"
   version "v0.0.1"
 
   if OS.mac?
     url "https://github.com/danielfoehrKn/kubectlSwitch/releases/download/0.0.1/switcher.tar.gz"
-    sha256 "327e964a2ea49ea7e00b5bac1d05ec557c921213eba1a56cadf79116a67cce10"
+    sha256 "f7ae5e8cf46d802cd3561cffb35c55e1ff66bb6b6fe4630ea651fa23f74d6b6e"
   elsif OS.linux?
-    url "https://github.com/danielfoehrKn/kubectlSwitch/releases/download/0.0.1/switcher.tar.gz"
-    sha256 "327e964a2ea49ea7e00b5bac1d05ec557c921213eba1a56cadf79116a67cce10"
+    url "https://github.com/danielfoehrKn/kubectlSwitch/releases/download/0.0.2/switcher.tar.gz"
+    sha256 "f7ae5e8cf46d802cd3561cffb35c55e1ff66bb6b6fe4630ea651fa23f74d6b6e"
   end
 
   depends_on :arch => :x86_64

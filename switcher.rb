3ad0d6d80685ec500e2ef27546c05af1cf988c6d
Daniel Foehr <daniel.foehr@sap.com>
2020-05-30 22:50:31 +0200

    Fixing switcher binary release url.

--- switcher.rb
+++ switcher.rb
@@ -4,7 +4,7 @@ class Switcher < Formula
   version "v0.0.1"
 
   if OS.mac?
-    url "https://github.com/danielfoehrKn/kubectlSwitch/releases/download/0.0.1/switcher.tar.gz"
+    url "https://github.com/danielfoehrKn/kubectlSwitch/releases/download/0.0.2/switcher.tar.gz"
     sha256 "f7ae5e8cf46d802cd3561cffb35c55e1ff66bb6b6fe4630ea651fa23f74d6b6e"
   elsif OS.linux?
     url "https://github.com/danielfoehrKn/kubectlSwitch/releases/download/0.0.2/switcher.tar.gz"

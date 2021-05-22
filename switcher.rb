c8ac38194feb3aee42e2b1b91884963ca6e23b96
danielfoehrKn <danielfoehrKn@users.noreply.github.com>
2021-05-22 11:29:14 +0000

    [create-pull-request] automated change

--- switcher.rb
+++ switcher.rb
@@ -1,14 +1,14 @@
 class Switcher < Formula
   desc "Providing the binary executable for the 'switch' script."
   homepage "https://github.com/danielfoehrKn/kubeswitch"
-  version "0.4.4"
+  version "0.4.5"
 
   if OS.mac?
-    url "https://github.com/danielfoehrKn/kubeswitch/releases/download/0.4.4/switcher_darwin_amd64"
-    sha256 "65d301b0493e2d035e144023e3ebae7ab11334b83949b1b99c4a842e71ab69e8"
+    url "https://github.com/danielfoehrKn/kubeswitch/releases/download/0.4.5/switcher_darwin_amd64"
+    sha256 "c503537c7d574cc900f7522fccf413e5059e6c2a99cac8dc72372adb331bb83d"
   elsif OS.linux?
-    url "https://github.com/danielfoehrKn/kubeswitch/releases/download/0.4.4/switcher_linux_amd64"
-    sha256 "74ecb8ee668cadfcef871928c53a31a834369cbb6bb21f4be9e88a0a0006dd7d"
+    url "https://github.com/danielfoehrKn/kubeswitch/releases/download/0.4.5/switcher_linux_amd64"
+    sha256 "6238d7f048eab9c35bb17556791053286cf9c84975bc4a1140f7b690168d1436"
   end
 
   depends_on :arch => :x86_64

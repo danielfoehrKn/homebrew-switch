96c50ddd39acf73d068880abc6e360f006ca02f3
danielfoehrKn <danielfoehrKn@users.noreply.github.com>
2021-12-13 23:14:37 +0000

    [create-pull-request] automated change

--- switch.rb
+++ switch.rb
@@ -1,9 +1,9 @@
 class Switch < Formula
   desc "The kubectx for operators."
   homepage "https://github.com/danielfoehrKn/kubeswitch"
-  version "0.5.0"
-  url "https://github.com/danielfoehrKn/kubeswitch/releases/download/0.5.0/switch.sh"
-  sha256 "3a629c2d1065adccbf636eb1c9f86884bad336d4d7ad06424801f461c16c6c4e"
+  version "0.6.0"
+  url "https://github.com/danielfoehrKn/kubeswitch/releases/download/0.6.0/switch.sh"
+  sha256 "93c131f9b8103604bd840d62cd2c5086be63da5454e73789e49bddf9f238088f"
 
   depends_on "danielfoehrkn/switch/switcher"
   depends_on :arch => :x86_64
--- switcher.rb
+++ switcher.rb
@@ -1,22 +1,32 @@
 class Switcher < Formula
   desc "Providing the binary executable for the 'switch' script."
   homepage "https://github.com/danielfoehrKn/kubeswitch"
-  version "0.5.0"
+  version "0.6.0"
 
   if OS.mac?
-    url "https://github.com/danielfoehrKn/kubeswitch/releases/download/0.5.0/switcher_darwin_amd64"
-    sha256 "c17aa2d0ad5d09a4a38450d26502a5b1a01bdc2bfe661fa9b8c70789e955d704"
+    if Hardware::CPU.arm?
+      url "https://github.com/danielfoehrKn/kubeswitch/releases/download/0.6.0/switcher_darwin_arm64"
+      sha256 "974277bbd75e2aea0d791fa2f5290ae13f62f1c778b635eb672f454255046bf2"
+    else
+      url "https://github.com/danielfoehrKn/kubeswitch/releases/download/0.6.0/switcher_darwin_amd64"
+      sha256 "91c840ff29447506d83c4d1f259d881bf1d6ee55ed3d8fed2bc5fa72fcc6e561"
+    end
   elsif OS.linux?
-    url "https://github.com/danielfoehrKn/kubeswitch/releases/download/0.5.0/switcher_linux_amd64"
-    sha256 "dc0c487b270755dc7fddf1b2dcceb54642067da42f4b0f25022d3ce1318b26b8"
+    url "https://github.com/danielfoehrKn/kubeswitch/releases/download/0.6.0/switcher_linux_amd64"
+    sha256 "4c61f804c9a0f11d8ab9636680fb7053715e2d52f191e84e1d7f4e073585b4f0"
   end
 
   depends_on :arch => :x86_64
 
   def install
       if OS.mac?
-        bin.install "switcher_darwin_amd64"
-        mv bin/"switcher_darwin_amd64", bin/"switcher"
+        if Hardware::CPU.arm?
+          bin.install "switcher_darwin_arm64"
+          mv bin/"switcher_darwin_arm64", bin/"switcher"
+        else
+          bin.install "switcher_darwin_amd64"
+          mv bin/"switcher_darwin_amd64", bin/"switcher"
+        end
       elsif OS.linux?
         bin.install "switcher_linux_amd64"
         mv bin/"switcher_linux_amd64", bin/"switcher"

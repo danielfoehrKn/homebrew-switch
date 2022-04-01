a8a81f8aa549221ac9aa160b112f0fe2ec221c61
danielfoehrKn <danielfoehrKn@users.noreply.github.com>
2022-04-01 12:35:44 +0000

    [create-pull-request] automated change

--- switcher.rb
+++ switcher.rb
@@ -1,19 +1,19 @@
 class Switcher < Formula
   desc "Providing the binary executable for the 'switch' script."
   homepage "https://github.com/danielfoehrKn/kubeswitch"
-  version "0.6.1"
+  version "0.6.2"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/danielfoehrKn/kubeswitch/releases/download/0.6.1/switcher_darwin_arm64"
-      sha256 "3284bae635a123751e6b6a3655e2180b93ddeadb0ae222a6448fb6e916a44a48"
+      url "https://github.com/danielfoehrKn/kubeswitch/releases/download/0.6.2/switcher_darwin_arm64"
+      sha256 "9f27d6769528e60d13ad6ccd6e09f471dabd808e9f0c24eda9e3452f85dfb4a2"
     else
-      url "https://github.com/danielfoehrKn/kubeswitch/releases/download/0.6.1/switcher_darwin_amd64"
-      sha256 "fc7251b5dacea822f942e424b06925f3def57060ba6627f8ed19ec79128568a3"
+      url "https://github.com/danielfoehrKn/kubeswitch/releases/download/0.6.2/switcher_darwin_amd64"
+      sha256 "7ed084c6d08f76ecfd752c2969fbc219eb7dfeeb0ab80792336ac8513ec1b49c"
     end
   elsif OS.linux?
-    url "https://github.com/danielfoehrKn/kubeswitch/releases/download/0.6.1/switcher_linux_amd64"
-    sha256 "17759a0e43d342716567f6ca602f3168efc8d1d036dbab79dfba2054d14b318c"
+    url "https://github.com/danielfoehrKn/kubeswitch/releases/download/0.6.2/switcher_linux_amd64"
+    sha256 "c93273e3ec228e65e68a669d48ce142acd5343c326ae519a7f3e2e124dc8aecb"
   end
 
   depends_on :arch => :x86_64

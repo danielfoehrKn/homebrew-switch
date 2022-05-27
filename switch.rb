a8be713ab17ff5a4a65253327b409994933ba954
danielfoehrKn <danielfoehrKn@users.noreply.github.com>
2022-05-27 11:47:22 +0000

    [create-pull-request] automated change

--- switch.rb
+++ switch.rb
@@ -1,11 +1,12 @@
 class Switch < Formula
   desc "The kubectx for operators."
   homepage "https://github.com/danielfoehrKn/kubeswitch"
-  version "0.6.2"
-  url "https://github.com/danielfoehrKn/kubeswitch/releases/download/0.6.2/switch.sh"
-  sha256 "93c131f9b8103604bd840d62cd2c5086be63da5454e73789e49bddf9f238088f"
+  version "0.7.0"
+  url "https://github.com/danielfoehrKn/kubeswitch/releases/download/0.7.0/switch.sh"
+  sha256 "20f673e399984deee681fa4bb3f1991ee5f3b710ce8a4bc8dc2fc7414743d04a"
 
   depends_on "danielfoehrkn/switch/switcher"
+  depends_on :arch => :x86_64
 
   def install
     prefix.install "switch.sh"
